d5a1bc710c3c997a5a6e90c5196f426632a616df
John Kevid <johnkevidma@gmail.com>
2020-09-07 16:03:26 -0300

    second commit

---
+++ src/util/request.ts
@@ -0,0 +1,13 @@
+import Axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+
+export interface RequestConfig extends AxiosRequestConfig {}
+
+export interface Response<T=any> extends AxiosResponse<T> {}
+
+export class Request{
+    constructor(private request = Axios){}
+
+    public get<T>(url: string, config: RequestConfig = {}): Promise<Response<T>>{
+        return this.request.get<T, Response<T>>(url, config);
+    };
+}
